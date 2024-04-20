031ba272c15d69f71dcb0502b21e1165902d2cec
hananeJchami <99352940+hananeJchami@users.noreply.github.com>
2024-04-20 19:27:34 +0200

    finalizing JsonSchemaManager methods

--- src/main/java/org/example/JsonSchemaManager.java
+++ src/main/java/org/example/JsonSchemaManager.java
@@ -10,16 +10,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class JsonSchemaManager {
-    private static final String SCHEMA_FILE_PATH = "path/to/your/schema.json";
-
-    /**
-     * Validates a JSON file against the predefined JSON schema.
-     * @param jsonFilePath path to the JSON file to be validated
-     */
-    public void validateJson(String jsonFilePath) throws IOException {
+    private static final String SCHEMA_FILE_PATH = "D:/Hanane/APP5/mail-client_json-schema/src/main/Schematics/Message_Schema.json";
+    public boolean validateJson(String jsonFilePath) throws IOException {
         File schemaFile = new File(SCHEMA_FILE_PATH);
         File jsonFile = new File(jsonFilePath);
 
@@ -27,14 +21,10 @@ public class JsonSchemaManager {
         JSONObject jsonSubject = new JSONObject(new JSONTokener(Files.newInputStream(jsonFile.toPath())));
 
         Schema schema = SchemaLoader.load(jsonSchema);
-        schema.validate(jsonSubject);  // throws a ValidationException if this object is invalid
+        schema.validate(jsonSubject);
+        return true;
     }
 
-    /**
-     * Reads a JSON file and returns it as a JsonNode.
-     * @param jsonFilePath path to the JSON file
-     * @return JsonNode representation of the JSON file
-     */
     public JsonNode readJson(String jsonFilePath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readTree(new File(jsonFilePath));
@@ -42,11 +32,14 @@ public class JsonSchemaManager {
 
     /**
      * Writes a JsonNode to a file.
-     * @param jsonNode JsonNode to write
+     *
+     * @param jsonNode     JsonNode to write
      * @param jsonFilePath path where to write the JSON file
+     * @return
      */
-    public void writeJson(JsonNode jsonNode, String jsonFilePath) throws IOException {
+    public boolean writeJson(JsonNode jsonNode, String jsonFilePath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.writerWithDefaultPrettyPrinter().writeValue(new FileWriter(jsonFilePath), jsonNode);
+        return true;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import javax.sound.midi.Soundbank;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String sample = "D:/Hanane/APP5/data-project_mail-client/test/test.json";
+        JsonSchemaManager test = new JsonSchemaManager();
+        System.out.println(test.validateJson(sample));
+        JsonNode node = test.readJson(sample);
+        System.out.println(node);
+        System.out.println(test.writeJson(node, sample));
+
     }
 }
